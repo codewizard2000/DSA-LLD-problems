2b2e9b127e0a96e59ab70f4fe52e264f81fe37dd
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-11-23 00:35:22 -0800

    Update PizzaBase.java

--- DecoratorDesignPattern/PizzaBase.java
+++ DecoratorDesignPattern/PizzaBase.java
@@ -1,5 +1,5 @@
 package DecoratorDesignPattern;
-
+// this is a part of structural design patter where base will be same we will keep decorating the top 
 public abstract class PizzaBase {
     public abstract int cost();// this is a base class
 }
