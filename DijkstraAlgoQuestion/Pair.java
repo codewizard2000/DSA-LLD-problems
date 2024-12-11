4f0e74ae299c6766d417d878e78d29bb0ad51297
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-12-11 18:12:01 +0530

    Add files via upload

---
+++ DijkstraAlgoQuestion/Pair.java
@@ -0,0 +1,10 @@
+package DijkstraAlgoQuestion;
+
+public class Pair {
+    int node;
+    int distance;
+    public Pair(int distance,int node){
+        this.node = node;
+        this.distance = distance;
+    }
+}
