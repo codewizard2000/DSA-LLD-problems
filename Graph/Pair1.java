e3072931065d33751d1259cf3a02665717d0d57a
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-12-11 16:40:23 +0530

    Add files via upload

---
+++ Graph/Pair1.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class Pair1 {
+    String first;
+    int second;
+
+
+    Pair1(String _first, int _second) {
+        this.first = _first;
+        this.second = _second;
+    }
+}
