6995de237c66b07393a49dcab2a9d56d993717ff
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-12-12 19:07:27 +0530

    Add files via upload

---
+++ DijkstraAlgoQuestion/Pair1.java
@@ -0,0 +1,11 @@
+package DijkstraAlgoQuestion;
+
+public class Pair1 {
+    int first;
+    int second;
+    public Pair1(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+
+}
