045989518836fc5809cf3fe0820b7dd4d39ddffd
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-12-13 20:20:19 +0530

    Delete Lambdas directory

--- Lambdas/Helloworld.java
+++
@@ -1,8 +0,0 @@
-package Lambdas;
-//way to create object of class that implements functinal interface
-public class Helloworld implements Runnable{
-    @Override
-    public void run() {
-        System.out.println("Hello world");
-    }
-}
