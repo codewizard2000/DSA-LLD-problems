d7398e55c1c3c8bbd511d24dc9e75265f59fbf0b
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2024-12-16 18:24:08 +0530

    Add files via upload

---
+++ ImpStringArrayQuestion/RotateMatrixby90.java
@@ -0,0 +1,37 @@
+package ImpStringArrayQuestion;
+
+public class RotateMatrixby90 {
+    public static int[][] rotate(int[][] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            for (int j = 0; j < arr.length / 2; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[i][arr.length - 1 - j];
+                arr[i][arr.length - 1 - j] = temp;
+            }
+        }
+        return arr;
+
+    }
+    public static void main(String args[]) {
+        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        rotate(arr);
+        System.out.println("Rotated Image");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+}
