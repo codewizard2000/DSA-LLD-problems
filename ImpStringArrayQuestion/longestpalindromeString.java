c3322965a889f62b1f8d73d1cfe2d032ca245326
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2025-01-14 18:00:48 +0530

    Update longestpalindromeString.java

--- ImpStringArrayQuestion/longestpalindromeString.java
+++ ImpStringArrayQuestion/longestpalindromeString.java
@@ -1,44 +1,31 @@
-package ImpStringArrayQuestion;
-
 public class longestpalindromeString {
-    int start;
-    int end;
-     public  String longest(String s){
-         start=0;
-         end=0;
-         int n=s.length();
-         for(int i=0;i<n-1;i++ ){
-             int p1=i;
-             int p2=i+1;
-             expand(s,p1,p2);
-         }
-
-         for(int i=1;i<n-1;i++){
-             int p1=i-1;
-             int p2=i+1;
-             expand(s,p1,p2);
-         }
-
-         return s.substring(start,end+1);
-    }
-
-    public void expand(String str,int p1,int p2){
-         while(p1>=0 && p2<str.length() && str.charAt(p1)==str.charAt(p2)){
-             p1--;
-             p2++;
-
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); // Odd length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
         }
-         if(p2-p1-1>end-start+1){
-             start=p1+1;
-             end=p2-1;
-         }
+        return s.substring(start, end + 1);
     }
 
-    public static void main(String[] args){
-        String str = "aaaabaaa";
-        longestpalindromeString H = new longestpalindromeString();
-        String result = H.longest(str);
-        System.out.println(result);
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right <= s.length()-1 && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+    }
+        return right - left - 1;
+    }
+    public static void main(String[] args) {
+        String str = "forgeeksskeegfor";
+        System.out.println("Longest Palindromic Substring: " + longestPalindrome(str)); // Output: "bab" or "aba"
 
     }
+
+
 }
