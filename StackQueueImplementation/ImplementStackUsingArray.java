36458c45c2575453afb9a60fbf4233cb3299c73a
Rishav Raj <77200564+codewizard2000@users.noreply.github.com>
2026-01-15 00:21:03 +0530

    Update ImplementStackUsingArray.java

--- StackQueueImplementation/ImplementStackUsingArray.java
+++ StackQueueImplementation/ImplementStackUsingArray.java
@@ -1,64 +1,39 @@
 package StackQueueImplementation;
 
-public class ImplementStackUsingArray {
-    public int top;
-    public int[] stackArray;
+public class StackUsingArray {
+    int[] stack;
+    int top;
     int size;
 
-    public ImplementStackUsingArray(int size) {
+    public StackUsingArray(int size) {
         this.size = size;
-        this.top = -1;
-        this.stackArray = new int[size];
+        stack = new int[size];
+        top = 0;   // stack is empty
     }
 
     public void push(int value) {
-        if (top == size - 1) {
-            System.out.println("Stack is full");
+        if (top == size) {
+            System.out.println("Stack overflow");
             return;
-        } else {
-            top = top + 1;
-            stackArray[top] = value;
         }
+        stack[top++] = value;
     }
 
     public int pop() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
+        if (top == 0) {
+            System.out.println("Stack underflow");
             return -1;
-        } else {
-            int value = stackArray[top]; // Retrieve the top element
-            top = top - 1;               // Decrement the top index
-            System.out.println("Popped element: " + value); // Print the popped element
-            return value;                // Return the retrieved element
         }
+        return stack[--top];
     }
 
     public int peek() {
-        if (top == -1) {
-            System.out.println("Stack is empty");
+        if (top == 0) {
+            System.out.println("Stack empty");
             return -1;
-        } else {
-            return stackArray[top];
         }
-    }
-
-    public static void main(String[] args) {
-        ImplementStackUsingArray stack = new ImplementStackUsingArray(5); // Create a stack of size 5
-
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        stack.push(40);
-        stack.push(50);
-
-        System.out.println("Top element is: " + stack.peek());
-
-        // Pop an element and check the top element
-        stack.pop();
-        System.out.println("Top element after pop: " + stack.peek());
-
-        // Push a new element and check the top element
-        stack.push(70);
-        System.out.println("Top element is: " + stack.peek());
+        return stack[top - 1];
     }
 }
+
+
